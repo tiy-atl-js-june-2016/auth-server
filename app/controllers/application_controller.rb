70fbcad5d85cb2d69b91d804a91bf5cdea99a20a
Brit Butler <redline6561@gmail.com>
2016-07-25 23:51:53 -0400

    Return a JSON error on 404.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { errors: e.message }, status: :not_found
+  end
 end
