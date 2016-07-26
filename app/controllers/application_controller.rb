1c9fab4add556d989117c97fbbc93e1f04653e06
Brit Butler <redline6561@gmail.com>
2016-07-25 23:55:05 -0400

    Add basic auth methods to Application Controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
+  def current_user
+    token = request.headers["Access-Token"]
+    if token
+      @user ||= User.find_by(access_token: token)
+    end
+  end
+
+  def authenticate!
+    unless current_user
+      render json: { errors: "You need to log in." }, status: :unauthorized
+    end
+  end
+
   rescue_from ActiveRecord::RecordNotFound do |e|
     render json: { errors: e.message }, status: :not_found
   end
